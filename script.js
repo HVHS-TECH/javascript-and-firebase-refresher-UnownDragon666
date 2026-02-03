a7db5fe2406b6fa118cf8dc32144f2caeac09c52
UnownDragon666 <unown.idrees@gmail.com>
2026-02-04 11:43:14 +1300

    Button

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log("Hello");
-document.getElementById("message").innerHTML = "Javascript connected!";   
+document.getElementById("message").innerHTML = "Javascript connected!";
+
+function buttonClick() {
+    document.getElementById("message").innerHTML = "Button pressed."
+}
